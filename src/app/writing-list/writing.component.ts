c75e870144ce89ad3ec5095497430e7f2a5de3af
nickjvargo <nickjvargo@gmail.com>
2021-07-18 17:33:33 -0700

    committing

--- src/app/writing-list/writing.component.ts
+++ src/app/writing-list/writing.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IStory } from './story';
+import { StoryService } from './story.service';
 
 @Component({
-  selector: 'nw-writing',
   templateUrl: './writing.component.html',
   styleUrls: ['./writing.component.css']
 })
 export class WritingComponent implements OnInit {
+  story: IStory | undefined;
+  errorMessage: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private storyService: StoryService) {
+  }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.getStory(id);
+    }
+  }
+
+  getStory(id: number): void {
+    this.storyService.getStory(id).subscribe({
+      next: story => this.story = story,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/stories']);
   }
 
 }
